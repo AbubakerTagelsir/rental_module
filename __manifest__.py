2d4a5a91fea85dbe9911921472351dca4661b3af
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-05-30 12:39:59 +0400

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Rental Module",
+
+    'summary': """
+        Real Estate Rent Management""",
+
+    'description': """
+        This module provide rental system for real estate properties.
+    """,
+
+    'author': "Abubaker Tagelsir <abubaker.tagelsir>",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Others',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'application': True,
+    'installable': True
+}
